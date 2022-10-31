bd8e26986775422d10d64e8cfb0a39e9d029eaa4
djoldes <davejoldes@gmail.com>
2022-10-31 18:41:04 +0200

    17

--- Setul_1/Program.cs
+++ Setul_1/Program.cs
@@ -1,5 +1,7 @@
 ﻿
 using System.Net.NetworkInformation;
+using System.Reflection.Metadata;
+using System.Runtime.Intrinsics.X86;
 
 int ui;
 Console.WriteLine("Introduceti numarul problemei:"); 
@@ -34,8 +36,275 @@ ui =int.Parse(Console.ReadLine());
         Console.WriteLine($"Ati accesat problema {ui}");
         p6();
     }
+    if (ui == 7)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p7();
+    }
+    if (ui == 8)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p8();
+    }
+    if (ui == 9)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p9();
+    };
+    if(ui == 10)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p10();
+    }
+    if (ui == 11)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p11();
+    }
+    if (ui == 12)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p12();
+    }
+    if (ui == 13)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p13();
+    }
+    if (ui == 14)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p14();
+    }
+    if (ui == 15)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p15();
+    }
+    if (ui == 16)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p16();
+    }
+    if (ui == 17)
+    {
+        Console.WriteLine($"Ati accesat problema {ui}");
+        p17();
+    }
 
-    void p6()
+void p17()
+{
+    int n=int.Parse(Console.ReadLine());
+    int m = int.Parse(Console.ReadLine());
+    while (n != m)
+        if (n > m)
+            n -= m;
+        else
+            m -= n;
+    Console.WriteLine($"Cel mai mare divizor comun este: {n}");
+}
+
+void p16()
+{
+    int a=int.Parse(Console.ReadLine());
+    int b=int.Parse(Console.ReadLine());
+    int c=int.Parse(Console.ReadLine());
+    int d=int.Parse(Console.ReadLine());
+    int e=int.Parse(Console.ReadLine());
+    int aux = 0;
+    if (a > b)
+    {
+        aux = a;
+        a = b;
+        b = aux;
+    }
+    if (a > c)
+    {
+        aux = a;
+        a = c;
+        c = aux;
+    }
+    if (a > d)
+    {
+        aux = a;
+        a = d;
+        d = aux;
+    }
+    if (a > e)
+    {
+        aux = a;
+        a = e;
+        e = aux;
+    }
+    if (b > c)
+    {
+        aux = b;
+        b = c;
+        c = aux;
+    }
+    if (b > d)
+    {
+        aux = b;
+        b = d;
+        d = aux;
+    }
+    if (b > e)
+    {
+        aux = b;
+        b = e;
+        e = aux;
+    }
+
+    if (c > d)
+    {
+        aux = c;
+        c = d;
+        d = aux;
+    }
+    if (c > e)
+    {
+        aux = c;
+        c = e;
+        e = aux;
+    }
+    if (d > e)
+    {
+        aux = d;
+        d = e;
+        e = aux;
+    }
+    Console.WriteLine(a + " " + b + " " + c + " " + d + " " + e);
+}
+
+void p15()
+{
+    int a = int.Parse(Console.ReadLine());
+    int b=int.Parse(Console.ReadLine());
+    int c = int.Parse(Console.ReadLine());
+    int aux = 0;
+    if (a > b )
+    {
+        aux = a;
+        a = b;
+        b = aux;
+    }
+    if (b > c)
+    {
+        aux = b;
+        b = c;
+        c = aux;
+    }
+    Console.WriteLine(a + " " + b + " " + c);
+}
+
+void p14()
+{
+    int n=int.Parse(Console.ReadLine());
+    int inv = 0;
+    int a = n;
+    while (n != 0)
+    {
+        inv = inv * 10 + n % 10;
+        n=n/ 10;
+    }
+    if (inv == a)
+    {
+        Console.WriteLine("Numarul este palindrom");
+    }
+    else
+        Console.WriteLine("Numarul nu este palindrom");
+}
+
+void p13()
+{
+    int y1=int.Parse(Console.ReadLine());
+    int y2=int.Parse(Console.ReadLine());
+    int nr = 0;
+    for(int i = y1; i <= y2; i++)
+    {
+        if (i % 4 == 0 && i % 100 != 0 && i%400!=0)
+        {
+            nr++;
+        }
+    }
+    Console.WriteLine(nr);
+}
+
+void p12()
+{
+    int nr = 0;
+    int a=int.Parse(Console.ReadLine());
+    int b=int.Parse(Console.ReadLine());
+    int n=int.Parse(Console.ReadLine());
+    for(int i=a; i<=b;i++)
+    {
+        if (i % n == 0)
+            nr++;
+    }
+    Console.WriteLine(nr);
+}
+
+void p11()
+{
+    int n = int.Parse(Console.ReadLine());
+    while(n > 0)
+    {
+        Console.WriteLine(n % 10);
+        n = n / 10;
+    }
+}
+
+void p10()
+{
+    int n=int.Parse(Console.ReadLine());
+    if (n == 2)
+    {
+        Console.WriteLine("Este prim");
+    }
+    if(n == 1)
+    {
+        Console.WriteLine("Nu este prim");
+    }
+    for (int d = 2; d <= Math.Sqrt(n); d++)
+    {
+        if (n % d == 0)
+        {
+            Console.WriteLine("Nu este prim");
+        }
+    }
+}
+
+void p9()
+{
+    int n = int.Parse(Console.ReadLine());
+    for (int d = 2; d <= n; d++)
+    {
+        if (n % d == 0)
+            Console.WriteLine(d);
+    }
+}
+
+void p8()
+{
+    int a = int.Parse(Console.ReadLine());
+    int b = int.Parse(Console.ReadLine());
+    a = a + b;
+    b = a - b;
+    a = a - b;
+    Console.WriteLine($"{a}, {b}");
+}
+
+void p7()
+    {
+        int a = int.Parse(Console.ReadLine());
+        int b = int.Parse(Console.ReadLine());
+        int aux = a;
+        a = b;
+        b = aux;
+        Console.WriteLine($"{a}, {b}");
+    }
+
+void p6()
     {
         int a, b, c;
         a = int.Parse(Console.ReadLine());
